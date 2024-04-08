ff26428bab42c4d000e75e17e4fb026b5fc0d428
txuseef <tauseef.shaikh8625@gmail.com>
2024-04-08 13:14:58 +0530

    Commit

---
+++ Java/src/Program/PersonRBCA22236.java
@@ -0,0 +1,22 @@
+package Program;
+
+public class PersonRBCA22236 {
+	    private String name;
+	    private int age;
+
+	    public PersonRBCA22236 (String name, int age) {
+	        this.name = name;
+	        this.age = age;
+	    }
+
+	    public String getName() {
+	        return name;
+	    }
+
+	    public int getAge() {
+	        return age;
+	    }
+	}
+
+
+
