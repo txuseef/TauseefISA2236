73016b9ab0c1c93484e70fd30e7c36b34f552f9c
txuseef <tauseef.shaikh8625@gmail.com>
2024-04-08 13:21:29 +0530

    Commit

---
+++ Java/src/Program/DogRBCA22236.java
@@ -0,0 +1,30 @@
+package Program;
+
+public class DogRBCA22236 {
+	    private String name;
+	    private String breed;
+
+	    public DogRBCA22236(String name, String breed) {
+	        this.name = name;
+	        this.breed = breed;
+	    }
+
+	    public String getName() {
+	        return name;
+	    }
+
+	    public void setName(String name) {
+	        this.name = name;
+	    }
+
+	    public String getBreed() {
+	        return breed;
+	    }
+
+	    public void setBreed(String breed) {
+	        this.breed = breed;
+	    }
+	}
+
+
+
