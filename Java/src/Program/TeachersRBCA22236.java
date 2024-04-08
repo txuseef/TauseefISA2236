f3a568aee93fff036a57bd62988f7fe40a4aeb25
txuseef <tauseef.shaikh8625@gmail.com>
2024-04-08 14:22:38 +0530

    Commit

---
+++ Java/src/Program/TeachersRBCA22236.java
@@ -0,0 +1,17 @@
+package Program;
+
+public class TeachersRBCA22236 {
+	    protected String type;
+
+	    // Constructor
+	    public TeachersRBCA22236 (String type) {
+	        this.type = type;
+	    }
+
+	    // Method to print what the teacher teaches
+	    public void teaches() {
+	        System.out.println("The teacher is teaching " + type);
+	    }
+	}
+
+
