3dadd6706ab79487502e85e199a015f0f106d06c
txuseef <tauseef.shaikh8625@gmail.com>
2024-04-08 13:38:00 +0530

    Commit

---
+++ Java/src/Program/VehicleRBCA22236.java
@@ -0,0 +1,20 @@
+package Program;
+
+public class VehicleRBCA22236 {
+	    private int horsepower;
+
+	    public VehicleRBCA22236 (int horsepower) {
+	        this.horsepower = horsepower;
+	    }
+
+	    public void drive() {
+	        System.out.println("Vehicle is being driven");
+	    }
+
+	    public int getHorsepower() {
+	        return horsepower;
+	    }
+	}
+
+
+
